c3dfd8319154c6e1a951931f159d777f52d489a8
Jenise Ruehle <jela07@gmail.com>
2020-01-28 21:59:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array = ["jenise", "hans", "jeron", "mom&dad","arrays!"]
+  array = ["wow", "jenise", "hans", "jeron", "mom&dad"]
   array.shift 
 end
 
