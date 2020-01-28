1aa964fe3ff517d91b4b49c81b1ac993cbfbadd8
Jenise Ruehle <jela07@gmail.com>
2020-01-28 21:56:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["jenise", "hans", "jeron", "mom&dad"]
-  array.pop
+  array.pop element
 end
 
 def remove_element_from_start_of_array(array)
